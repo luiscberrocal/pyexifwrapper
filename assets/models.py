57dad91a43fa5bf4ab2132a7022d4ef0cdf4ef6b
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2012-05-12 15:59:59 -0500

    Added Django capabilities.

---
+++ assets/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class AssetFolder(models.Model):
+    path = models.CharField(max_length = 512)
+    last_scanned = models.DateTimeField()
+    
+class Asset(models.Model):
+    filename = models.CharField(max_length=128)
+    folder = models.ForeignKey(AssetFolder)
+    file_date = models.DateTimeField()
+    file_size = models.DecimalField(max_digits=8, decimal_places=2)
